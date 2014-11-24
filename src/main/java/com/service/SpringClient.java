94a7d23551fe9a5467b26812e971dd5332bd1cac
jonas <jonas.lund@mobenga.com>
2014-11-24 13:26:37 +0100

    mooorran

--- src/main/java/com/service/SpringClient.java
+++ src/main/java/com/service/SpringClient.java
@@ -2,36 +2,47 @@ package com.service;
 
 import com.bean.RestTemplateBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.Identifier;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.Resource;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by jonas on 2014-11-21.
  */
+//TODO Cache lager
 @Component
 public class SpringClient {
     @Autowired
     private RestTemplateBean restTemplateBean;
-
-    public <T> T exchange(final String requestUrl, final Class<T> clazz){
-        try{
+    private LinkedHashMap<String, String> lolCache = new LinkedHashMap<String, String>();
+    public <T> String exchange(final String requestUrl, final Class<T> clazz){
+        String fromCache = getFromCache(requestUrl);
+        if(fromCache != null){
+            System.out.println("Returning from lolcache");
+          return fromCache;
+      }
+         try{
             ResponseEntity<T> responseEntity = restTemplateBean.getObject().exchange(requestUrl, HttpMethod.GET, null, clazz);
             if(null == responseEntity || null == responseEntity.getBody()){
                 System.out.println("Error calling url : "+requestUrl+", Http code : {} " + responseEntity != null ? (responseEntity.getStatusCode() != null ? responseEntity.getStatusCode() : null):null);
-                return clazz.newInstance();
+                return clazz.newInstance().toString();
             }
-            return responseEntity.getBody();
+            lolCache.put(requestUrl, responseEntity.getBody().toString());
+            return responseEntity.getBody().toString();
         }
         catch (Exception e){
             try {
-                return clazz.newInstance();
+                return clazz.newInstance().toString();
             } catch (Exception e1) {
             }
         }
         return null;
+    }
+
+    public <T> String getFromCache(String identifier) {
+       return lolCache.get(identifier);
     }
 }
